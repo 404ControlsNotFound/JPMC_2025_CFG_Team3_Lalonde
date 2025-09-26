42803af016199b89a3e36dacba7cde6b88985280
Lee Zheng Jing <96382278+leezhengjing@users.noreply.github.com>
2025-09-26 14:11:06 +0800

    chore: update frontend user schemas and componentalize things (#6)

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,11 +2,7 @@ import React from "react";
 
 import { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import {
-  createRootRouteWithContext,
-  Link,
-  Outlet,
-} from "@tanstack/react-router";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 
 const TanStackRouterDevtools =
   process.env.NODE_ENV === "production"
@@ -25,8 +21,8 @@ export const Route = createRootRouteWithContext<{
 }>()({
   component: () => (
     <>
-      <div className="flex h-[60px] gap-4 bg-blue-800 p-4 text-white shadow-md">
-        <Link
+      <div className="flex h-[60px] gap-4 bg-blue-800 p-2 text-white shadow-md">
+        {/* <Link
           to="/"
           className="transition-colors hover:text-blue-200 [&.active]:font-bold"
         >
@@ -37,7 +33,7 @@ export const Route = createRootRouteWithContext<{
           className="transition-colors hover:text-blue-200 [&.active]:font-bold"
         >
           Demo
-        </Link>
+        </Link> */}
       </div>
       <main className="h-[calc(100vh-60px)] w-screen bg-gray-900 text-white">
         <Outlet />
